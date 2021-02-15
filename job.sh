c27834d132cb1db2781b5d4c586432468e395770
johnryan465 <johnryan465@gmail.com>
2021-02-15 20:12:06 +0000

    Added job.sh

---
+++ job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --job-name="active-learning"
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs/scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate active_learning srun python main.py --name "vduq_bb_0.001" --description "Random vDUQ" --initial_per_class 2  --aquisition_size 5 batchbald  --batch_size 64 --epochs 100 vduq --lr 0.001
