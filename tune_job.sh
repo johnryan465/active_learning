817f0402e686b5eae44f6f7da8a9e688eada1e3c
johnryan465 <johnryan465@gmail.com>
2021-04-13 01:13:02 +0100

    Updated tune job

--- tune_job.sh
+++ tune_job.sh
@@ -3,6 +3,9 @@
 #SBATCH --gres=gpu:4
 #SBATCH --job-name="active-learning-tunning"
 #SBATCH --partition=msc
-sh create_env.sh
+export CONDA_ENVS_PATH=/scratch/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch/$USER/conda_pkgs/scratch-ssd/oatml/scripts/run_locked.sh
+/scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate active_learning
+pip install batchbald_redux==2.0.5 --no-deps
 python tune.py --data_path /scratch-ssd/oatml/data
