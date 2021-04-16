53326116ae4c7c3d06e1c8dc14105bfa4dad9aef
y0ast <git@joo.st>
2021-04-16 10:16:09 +0100

    fix slurm scripts

--- create_env.sh
+++ create_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CONDA_ENVS_PATH=/scratch/$USER/conda_envs
-export CONDA_PKGS_DIRS=/scratch/$USER/conda_pkgs/scratch-ssd/oatml/scripts/run_locked.sh
-/scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
+export CONDA_PKGS_DIRS=/scratch/$USER/conda_pkgs
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate active_learning
 pip install batchbald_redux==2.0.5 --no-deps
--- job.sh
+++ job.sh
@@ -3,8 +3,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --job-name="active-learning"
 #SBATCH --partition=msc_preemptable
+
 export CONDA_ENVS_PATH=/scratch/$USER/conda_envs
-export CONDA_PKGS_DIRS=/scratch/$USER/conda_pkgs/scratch-ssd/oatml/scripts/run_locked.sh
-/scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
+export CONDA_PKGS_DIRS=/scratch/$USER/conda_pkgs
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate active_learning
 python main.py --name "vduq_bb_0.001" --description "BatchBALD vDUQ" --initial_per_class 2  --aquisition_size 5 batchbald  --batch_size 64 --epochs 100 vduq --lr 0.001
