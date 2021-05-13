206ef528da2f3fa57cca0c90f36e59e730bd4a97
johnryan465 <johnryan465@gmail.com>
2021-05-13 15:29:45 +0100

    Plot confusion matrix

--- experimental/driver.py
+++ experimental/driver.py
@@ -8,13 +8,59 @@ from utils.config import IO, VariationalLoss
 from models.training import TrainingParams
 from ignite.handlers import ModelCheckpoint, Checkpoint
 from ignite.metrics.confusion_matrix import ConfusionMatrix
+import PIL.Image
+from torchvision.transforms import ToTensor
+import io
 
 from datasets.activelearningdataset import ActiveLearningDataset
 import time
 from ray import tune
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+import itertools
 
 class Driver:
+    @staticmethod
+    def plot_confusion_matrix(cm, class_names):
+        """
+        Returns a matplotlib figure containing the plotted confusion matrix.
+        
+        Args:
+        cm (array, shape = [n, n]): a confusion matrix of integer classes
+        class_names (array, shape = [n]): String names of the integer classes
+        """
+        
+        figure = plt.figure(figsize=(8, 8))
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title("Confusion matrix")
+        plt.colorbar()
+        tick_marks = np.arange(len(class_names))
+        plt.xticks(tick_marks, class_names, rotation=45)
+        plt.yticks(tick_marks, class_names)
+        
+        # Normalize the confusion matrix.
+        cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+        
+        # Use white text if squares are dark; otherwise black.
+        threshold = cm.max() / 2.
+        
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            color = "white" if cm[i, j] > threshold else "black"
+            plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+            
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        buf = io.BytesIO()
+        plt.savefig(buf, format='jpeg')
+        buf.seek(0)
+        
+        image = PIL.Image.open(buf)
+        image = ToTensor()(image)
+        return image
+
+
     @staticmethod
     def train(exp_name: str, iteration: int, training_params: TrainingParams, model_wrapper: ModelWrapper, dataset: ActiveLearningDataset, tb_logger) -> ModelWrapper:
         training_params = model_wrapper.get_training_params()
@@ -107,6 +153,8 @@ class Driver:
             evaluator.run(test_loader)
             line = dict(evaluator.state.metrics)
             print(line)
+            img = Driver.plot_confusion_matrix(line["confusion"].numpy(), [str(i) for i in range(10)])
+            tb_logger.writer.add_image("confusion", img, trainer.state.epoch)
             line['epoch'] = trainer.state.epoch
             test_log_lines.append(line)
 
