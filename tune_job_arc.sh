40d51acdd50621889e0aaea84abfbc9a6446bf4b
johnryan465 <johnryan465@gmail.com>
2021-05-22 17:14:51 +0100

    Fixed typo

--- tune_job_arc.sh
+++ tune_job_arc.sh
@@ -7,6 +7,6 @@ module load python/anaconda3/2020.11
 module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
-source activate $DATA/conda_envs/activate_learning
+source activate $DATA/conda_envs/active_learning
 
 python tune.py --data_path $DATA/data
